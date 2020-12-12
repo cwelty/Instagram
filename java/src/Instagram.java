8cedb81d2e99f6d4197894f5753559e250ff97ee
Christian Lee <clee244@ucr.edu>
2020-12-12 12:21:23 -0800

    testing case 4 update

--- java/src/Instagram.java
+++ java/src/Instagram.java
@@ -23,6 +23,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * This class defines a simple embedded SQL utility class that is designed to
@@ -44,6 +45,7 @@ public class Instagram{
 			
 			// obtain a physical connection
 	        this._connection = DriverManager.getConnection(url, user, passwd);
+	        this._connection.setAutoCommit(false); // ADDED |||||||||||||||||||||||||||||||
 	        System.out.println("Done");
 		}catch(Exception e){
 			System.err.println("Error - Unable to Connect to Database: " + e.getMessage());
@@ -59,15 +61,17 @@ public class Instagram{
 	 * @param sql the input SQL string
 	 * @throws java.sql.SQLException when update failed
 	 * */
-	public void executeUpdate (String sql) throws SQLException { 
+	public int executeUpdate (String sql) throws SQLException { 
 		// creates a statement object
 		Statement stmt = this._connection.createStatement ();
 
 		// issues the update instruction
-		stmt.executeUpdate (sql);
+		int val = stmt.executeUpdate (sql);
 
+		this._connection.commit();
 		// close the instruction
 	    stmt.close ();
+	    return val;
 	}//end executeUpdate
 
 	/**
@@ -155,24 +159,16 @@ public class Instagram{
 	 * method issues the query to the DBMS and returns the number of results
 	 * 
 	 * @param query the input query string
-	 * @return the number of rows returned
+	 * @return the ResultSet object of query
 	 * @throws java.sql.SQLException when failed to execute the query
 	 */
-	public int executeQuery (String query) throws SQLException {
+	public ResultSet executeQuery (String query) throws SQLException {
 		//creates a statement object
 		Statement stmt = this._connection.createStatement ();
 
 		//issues the query instruction
 		ResultSet rs = stmt.executeQuery (query);
-
-		int rowCount = 0;
-
-		//iterates through the result set and count nuber of results.
-		if(rs.next()){
-			rowCount++;
-		}//end while
-		stmt.close ();
-		return rowCount;
+		return rs;
 	}
 	
 	/**
@@ -212,6 +208,7 @@ public class Instagram{
 	 * @param args the command line arguments this inclues the <mysql|pgsql> <login file>
 	 */
 	public static void main (String[] args) {
+
 		if (args.length != 3) {
 			System.err.println (
 				"Usage: " + "java [-classpath <classpath>] " + Instagram.class.getName () +
@@ -239,6 +236,10 @@ public class Instagram{
 			String user = args[2];
 			
 			esql = new Instagram (dbname, dbport, user, "");
+
+			// Variables
+			Scanner input = new Scanner(System.in);
+			String value;
 			
 			boolean keepon = true;
 			while(keepon){
@@ -261,18 +262,89 @@ public class Instagram{
 				 * FOLLOW THE SPECIFICATION IN THE PROJECT DESCRIPTION
 				 */
 				switch (readChoice()){
-					case 1: UploadPhoto(esql); break;
-					case 2: DownloadPhoto(esql); break;
-					case 3: SearchForUser(esql); break;
-					case 4: FollowUser(esql); break;
-					case 5: GenerateNewsFeed(esql); break;
-					case 6: TagPhoto(esql); break;
-					case 7: SearchForPhotos(esql); break;
-					case 8: CommentOnPhoto(esql); break;
-					case 9: TagUserToPhoto(esql); break;
-					case 10: MostPopularUsers(esql); break;
-					case 11: MostPopularPhotos(esql); break;
-					case 12: keepon = false; break;
+					case 1: //UploadPhoto 
+
+					break;
+					
+					case 2: //DownloadPhoto
+
+					break;
+					
+					case 3: //SearchForUser
+						System.out.print("\nWould you like to search for User based on\n1) Photo Title\n2) Tags\n3) Ratings\nPlease enter an integer(1-3)\n");
+						value = input.nextLine();
+						int choice = Integer.parseInt(value);
+						if(choice < 1 || choice > 3) {
+							System.out.print("\nInvalid choice: Returning to menu.\n");
+							break;
+						}
+						else if(choice == 1) {
+							System.out.print("\nPlease enter the photo title: \n");
+							String title = input.nextLine();
+							System.out.print("\n");
+							SearchForUserTitle(esql, title);
+						}
+						else if(choice == 2) {
+							System.out.print("\nPlease enter the tag: \n");
+							String tag = input.nextLine();
+							System.out.print("\n");
+							SearchForUserTag(esql, tag);
+						}
+						else { //choice == 3
+							System.out.print("\nPlease enter a rating: \n");
+							value = input.nextLine();
+							int rating = Integer.parseInt(value);
+							System.out.print("\n");
+							SearchForUserRating(esql, rating);
+						}
+
+					break;
+					
+					case 4: //FollowUser(esql); 
+						System.out.print("\nPlease enter the username of the user you'd like to follow:\n");
+						value = input.nextLine();
+						if(UserExists(esql, value)) {
+							System.out.println("BEFORE FOLLOW USER\n");
+							FollowUser(esql, user, value);
+						}
+						else {
+							System.out.print("The user \"" + value + "\" does not exist. Returning to menu.\n"); 
+						}
+
+
+					break;
+					
+					case 5: //GenerateNewsFeed(esql); 
+
+					break;
+					
+					case 6: //TagPhoto(esql); 
+
+					break;
+					
+					case 7: //SearchForPhotos(esql); 
+
+					break;
+					
+					case 8: //CommentOnPhoto(esql); 
+
+					break;
+					
+					case 9: //TagUserToPhoto(esql); 
+
+					break;
+					
+					case 10: //MostPopularUsers(esql); 
+
+					break;
+					
+					case 11: //MostPopularPhotos(esql); 
+
+					break;
+					
+					case 12: keepon = false; 
+
+					break;
 				}
 			}
 		}catch(Exception e){
@@ -314,12 +386,67 @@ public class Instagram{
 		
 	}
 	
-	public static void SearchForUser(Instagram esql){//3
-		
+	public static void SearchForUserTitle(Instagram esql, String title){//3
+		try {
+			ResultSet rs = esql.executeQuery("SELECT user_id FROM Photo WHERE title = \'" + title + "\'");
+			if (rs.isBeforeFirst()){
+				System.out.println("Search Results for title \"" + title + "\":");
+				while(rs.next()){
+					String user_id = rs.getString(1);
+					System.out.println(user_id + "\n");
+				}
+			}else{
+				System.out.println("No results found. Returning to menu.\n");
+			}
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}	
 	}
 	
-	public static void FollowUser(Instagram esql){//4
-		
+	public static void SearchForUserTag(Instagram esql, String tag){//3
+		try {
+			ResultSet rs = esql.executeQuery("SELECT user_id FROM Photo WHERE parent_id IN (SELECT parent_id FROM PhotoTags WHERE tag = \'" + tag + "\')"); 
+			if (rs.isBeforeFirst()){
+				System.out.println("Search Results for tag \"" + tag + "\":");
+				while(rs.next()){
+					String user_id = rs.getString(1);
+					System.out.println(user_id + "\n");
+				}
+			}else{
+				System.out.println("No results found. Returning to menu.\n");
+			}
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}		
+	}
+
+	public static void SearchForUserRating(Instagram esql, int rating){//3
+		try {
+			ResultSet rs = esql.executeQuery("SELECT user_id FROM Photo WHERE rating = \'" + rating + "\'"); 
+			if (rs.isBeforeFirst()){
+				System.out.println("Search Results for rating \"" + rating + "\":");
+				while(rs.next()){
+					String user_id = rs.getString(1);
+					System.out.println(user_id + "\n");
+				}
+			}else{
+				System.out.println("No results found. Returning to menu.\n");
+			}
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}		
+	}
+
+	public static void FollowUser(Instagram esql, String follower, String follows){ // CURRENTLY NOT UPDATING CSV'S
+		try {
+			int following_id = GetNextFollowId(esql);
+			int changes = esql.executeUpdate("INSERT INTO Followings VALUES(\'" + following_id + "\', \'" + follower + "\', \'" + follows + "\')");
+			changes = changes + esql.executeUpdate("UPDATE Users SET follow_count = follow_count + 1 WHERE user_id = \'" + follows + "\'");
+			System.out.println("Changes = " + changes + "\n");
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
 	}
 	
 	public static void GenerateNewsFeed(Instagram esql) throws Exception{//5
@@ -351,6 +478,38 @@ public class Instagram{
 	public static void MostPopularPhotos(Instagram esql){//11
 		//
 		
+	}
+
+	//helper function for FollowUser()
+	public static boolean UserExists(Instagram esql, String user_id){
+		try {
+			ResultSet rs = esql.executeQuery("SELECT user_id FROM Users WHERE user_id = \'" + user_id + "\'");
+			if(rs.isBeforeFirst()) {
+				return true;
+			}
+			return false;
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return false;
+	}
+
+	//helper function to get FollowUser()
+	public static int GetNextFollowId(Instagram esql) {
+		try {
+			ResultSet rs = esql.executeQuery("SELECT following_id FROM Followings WHERE following_id = (SELECT MAX(following_id) FROM followings)");
+			if(rs.isBeforeFirst()) {
+				rs.next();
+				int following_id = rs.getInt(1) + 1;
+				return following_id;
+			}
+			else {
+				return 1;
+			}
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return -1;
 	}
 	
 }
--- sql/create.sql
+++ sql/create.sql
@@ -7,41 +7,67 @@ DROP TABLE IF EXISTS Tag;
 -- Entities
 
 CREATE TABLE Photo (
-    photo_id VARCHAR(64) NOT NULL,
-    parent_id VARCHAR(64) NOT NULL,
+    parent_id NUMERIC(4) NOT NULL,
     user_id VARCHAR(64) NOT NULL,
     title VARCHAR(64) NOT NULL,
-    ratings NUMERIC(5) NOT NULL,
+    rating NUMERIC(5) NOT NULL,
     dates VARCHAR(20) NOT NULL,
-    timer NUMERIC(4) NOT NULL,
-    PRIMARY KEY(photo_id)
+    time_stamp NUMERIC(4) NOT NULL,
+    --image NUMERIC(4) NOT NULL,
+    views NUMERIC(4) NOT NULL,
+    PRIMARY KEY(parent_id)
 );
 
 CREATE TABLE Comments (
-    comment_id VARCHAR(64) NOT NULL,
+    parent_id NUMERIC(4) NOT NULL,
+    comment_id NUMERIC(4) NOT NULL,
+    user_id VARCHAR(64) NOT NULL,
     content VARCHAR(1000) NOT NULL,
-    parent_id VARCHAR(64) NOT NULL,
     PRIMARY KEY(comment_id)
 );
 
 CREATE TABLE Users (
     user_id VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
+    follow_count NUMERIC(4) NOT NULL,
     PRIMARY KEY(user_id)
 );
 
 CREATE TABLE Rating (
-    rating_id VARCHAR(64) NOT NULL,
+    parent_id NUMERIC(4) NOT NULL,
+    rating_id NUMERIC(4) NOT NULL,
+    user_id VARCHAR(64) NOT NULL,
     rating_type VARCHAR(64) NOT NULL,
-    parent_id VARCHAR(64) NOT NULL,
     PRIMARY KEY(rating_id)
 );
 
-CREATE TABLE Tag (
-    tag_id VARCHAR(64) NOT NULL,
+CREATE TABLE PostRatings (
+    parent_id NUMERIC(4) NOT NULL,
+    post_rate_id NUMERIC(4) NOT NULL,
+    likes NUMERIC(4) NOT NULL,
+    dislikes NUMERIC(4) NOT NULL,
+    PRIMARY KEY(post_rate_id)
+);
+
+CREATE TABLE PhotoTags (
+    parent_id NUMERIC(4) NOT NULL,
+    photo_tag_id NUMERIC(4) NOT NULL,
     tag VARCHAR(64) NOT NULL,
-    parent_id VARCHAR(64) NOT NULL,
-    PRIMARY KEY(tag_id)
+    PRIMARY KEY(photo_tag_id)
+);
+
+CREATE TABLE UserTags (
+    parent_id NUMERIC(4) NOT NULL,
+    user_tag_id NUMERIC(4) NOT NULL,
+    user_id VARCHAR(64) NOT NULL,
+    PRIMARY KEY(user_tag_id)
+);
+
+CREATE TABLE Followings (
+    following_id NUMERIC(4) NOT NULL,
+    follower VARCHAR(64) NOT NULL,
+    follows VARCHAR(64) NOT NULL,
+    PRIMARY KEY(following_id)
 );
 
 -- Relations
@@ -51,42 +77,57 @@ CREATE TABLE Tag (
 -- INSERT DATA STATEMENTS --
 ----------------------------
 
-/*
+
 COPY Photo (
-    photo_id,
     parent_id,
     user_id,
     title,
-    tags,
-    ratings,
+    rating,
     dates,
-    timer
+    time_stamp,
+    views
 )
---FROM 'Photos.csv'
+FROM 'Photos.csv'
 WITH DELIMITER ',';
-*/
+
+COPY Rating (
+    parent_id,
+    rating_id,
+    user_id,
+    rating_type
+)
+FROM 'Ratings.csv'
+WITH DELIMITER ',';
+
+COPY Users (
+    user_id,
+    password,
+    follow_count
+)
+FROM 'Users.csv'
+WITH DELIMITER ',';
+
+COPY Followings (
+    following_id,
+    follower,
+    follows
+)
+FROM 'Followings.csv'
+WITH DELIMITER ',';
+
+/*
 COPY Comments (
     comment_id,
     content,
+    user_id,
     parent_id
 )
 FROM 'Comments.csv'
 WITH DELIMITER ',';
-/*
-COPY Users (
-    user_id,
-    password
-)
---FROM 'Users.csv'
-WITH DELIMITER ',';
 
-COPY Rating (
-    rating_id,
-    rating_type,
-    parent_id
-)
---FROM 'Rating.csv'
-WITH DELIMITER ',';
+
+
+
 
 COPY Tag  (
     tag_id,
@@ -96,3 +137,11 @@ COPY Tag  (
 --FROM 'Tag.csv'
 WITH DELIMITER ',';
 */
+
+COPY PhotoTags (
+    parent_id,
+    photo_tag_id,
+    tag
+)
+FROM 'PhotoTags.csv'
+WITH DELIMITER ',';
